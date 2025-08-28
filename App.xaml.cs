eb8f46413aa05a5de8f8f5eb1a8091f02c35271d
RobertoPombo <captn.sponge2@web.de>
2025-08-28 13:50:30 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LBV_GTM_Database_Viewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
